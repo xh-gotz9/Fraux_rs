1f971d4fb6ed615006efaa452a274344a1a046ac
xh-gotz9 <xh353127981@gmail.com>
2020-11-16 01:43:30 +0800

    修改 parse 方法的 Result 类型, 修改测试代码

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,7 @@ pub enum BData {
     Dict(Rc<HashMap<String, BData>>),
 }
 
-#[derive(Debug)]
-pub struct ParseErr {
-    message: String,
-}
-
-pub fn parse(s: &str) -> Result<BData, ParseErr> {
+pub fn parse(s: &str) -> Result<BData, String> {
     let mut peekable = s.chars().peekable();
     let c = peekable.peek().unwrap();
     let res = match c {
@@ -22,9 +17,7 @@ pub fn parse(s: &str) -> Result<BData, ParseErr> {
         'i' => parse_number(&mut peekable),
         'l' => parse_list(&mut peekable),
         'd' => parse_dict(&mut peekable),
-        _ => Err(ParseErr {
-            message: String::from("非法字符"),
-        }),
+        _ => return Err(String::from("非法字符")),
     };
 
     if peekable.peek().is_some() {
@@ -34,12 +27,19 @@ pub fn parse(s: &str) -> Result<BData, ParseErr> {
     res
 }
 
-fn parse_number(s: &mut Peekable<Chars<'_>>) -> Result<BData, ParseErr> {
-    if s.next().expect("没有数据") != 'i' {
-        return Err(ParseErr {
-            message: "解析错误".to_string(),
-        });
+fn parse_number(s: &mut Peekable<Chars<'_>>) -> Result<BData, String> {
+    let cv = s.next();
+    match cv {
+        Some(c) => {
+            if c != 'i' {
+                return Err("解析错误".to_string());
+            }
+        }
+        None => {
+            return Err("解析错误".to_string());
+        }
     }
+
     let mut symb = false;
     let mut num = String::from("");
     loop {
@@ -50,9 +50,7 @@ fn parse_number(s: &mut Peekable<Chars<'_>>) -> Result<BData, ParseErr> {
             }
             '+' | '-' => {
                 if symb {
-                    return Err(ParseErr {
-                        message: "格式错误".to_string(),
-                    });
+                    return Err("格式错误".to_string());
                 } else {
                     num.push(v);
                     symb = true;
@@ -60,9 +58,7 @@ fn parse_number(s: &mut Peekable<Chars<'_>>) -> Result<BData, ParseErr> {
             }
             'e' => break,
             _ => {
-                return Err(ParseErr {
-                    message: "解析错误".to_string(),
-                });
+                return Err("解析错误".to_string());
             }
         }
     }
@@ -71,22 +67,18 @@ fn parse_number(s: &mut Peekable<Chars<'_>>) -> Result<BData, ParseErr> {
     Ok(BData::Number(v))
 }
 
-pub fn parse_string(s: &mut Peekable<Chars<'_>>) -> Result<BData, ParseErr> {
+pub fn parse_string(s: &mut Peekable<Chars<'_>>) -> Result<BData, String> {
     let mut len = 0;
     loop {
-        let c = s.next().unwrap();
-        match c {
-            '0'..='9' => {
-                len = len * 10 + (c as u8 - b'0');
+        let v = s.next();
+        match v {
+            Some('0'..='9') => {
+                len = len * 10 + (v.unwrap() as u8 - b'0');
             }
-            ':' => {
+            Some(':') => {
                 break;
             }
-            _ => {
-                return Err(ParseErr {
-                    message: "格式错误".to_string(),
-                })
-            }
+            None | Some(_) => return Err("格式错误".to_string()),
         }
     }
 
@@ -94,47 +86,59 @@ pub fn parse_string(s: &mut Peekable<Chars<'_>>) -> Result<BData, ParseErr> {
     let mut bstr = String::from("");
 
     while i < len {
-        let c = s.next().unwrap();
-        bstr.push(c);
-        i += 1;
+        if let Some(c) = s.next() {
+            bstr.push(c);
+            i += 1;
+        } else {
+            return Err("格式错误".to_string());
+        }
     }
 
     Ok(BData::BString(bstr))
 }
 
-fn parse_list(_s: &mut Peekable<Chars<'_>>) -> Result<BData, ParseErr> {
+fn parse_list(_s: &mut Peekable<Chars<'_>>) -> Result<BData, String> {
     Ok(BData::List(Rc::new(vec![])))
 }
 
-fn parse_dict(_s: &Peekable<Chars>) -> Result<BData, ParseErr> {
+fn parse_dict(_s: &Peekable<Chars>) -> Result<BData, String> {
     Ok(BData::Dict(Rc::new(HashMap::new())))
 }
 
 #[cfg(test)]
 mod test {
-    fn parse_char(s: &str) -> String {
-        let v = super::parse(s).unwrap_or(super::BData::BString("".to_string()));
-        if let super::BData::BString(data) = v {
-            return data;
+    fn parse_bstring(s: &str) -> Result<String, &str> {
+        let v = super::parse(s);
+        if let Ok(super::BData::BString(data)) = v {
+            Ok(data)
         } else {
-            return "".to_string();
+            Err("err")
         }
-    }
-    #[test]
-    fn parse_string_test() {
-        assert_eq!(parse_char("3:abc"), "abc");
-        assert_eq!(parse_char("3:abcd"), "abc");
-        assert_eq!(parse_char("0:"), "");
     }
 
     #[test]
-    fn parse_number_test() {
-        let num = String::from("i322e");
-        let obj = super::parse(&num).expect("parse err");
-        if let super::BData::Number(v) = obj {
-            assert_eq!(v, 322);
-            return;
+    fn parse_bstring_test() {
+        assert_eq!(parse_bstring("3:abc"), Ok("abc".to_string()));
+        assert_eq!(parse_bstring("3:ab"), Err("err"));
+        assert_eq!(parse_bstring("3:abcd"), Ok("abc".to_string()));
+        assert_eq!(parse_bstring("0:"), Ok("".to_string()));
+        assert_eq!(parse_bstring("-1:"), Err("err"));
+    }
+
+    fn parse_num(s: &str) -> Result<i32, &str> {
+        let v = super::parse(s);
+        if let Ok(super::BData::Number(data)) = v {
+            Ok(data)
+        } else {
+            Err("err")
         }
-        panic!("parse err");
+    }
+
+    #[test]
+    fn parse_num_test() {
+        assert_eq!(parse_num("i32e"), Ok(32));
+        assert_eq!(parse_num("i-32e"), Ok(-32));
+        assert_eq!(parse_num("i0e"), Ok(0));
+        assert_eq!(parse_num("i3.2e"), Err("err"));
     }
 }
